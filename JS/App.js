54bc964d0025b2659d963fb702e74148e62995a9
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2024-01-22 21:51:49 +0530

    Add files via upload

--- JS/App.js
+++ JS/App.js
@@ -5,85 +5,242 @@ function start() {
     const attacks = document.getElementById("attacks")
     const time = document.getElementById("time")
     const sites = document.getElementById("sites")
+    document.querySelector("button").innerText = 'Stop'
+    document.querySelector("button").onclick = cancel
     document.getElementById("st").style.borderLeft = '2px solid yellow'
-    status.innerText = "🟢 ON" 
-    setTimeout(() => {
-        status.innerText = "⚡ Attacking"
-        
-        attack()
-    }, 1000)
-} 
-function attack() {
-     document.getElementById("st").style.borderLeft = '2px solid green'
-    const link = document.getElementById("url").value;
+    status.innerText = "🟢 ON"  
+    n = document.querySelector('.sss')
+                window.scroll(0, screenTop);   
+    n.innerHTML = `
+    <div class="n" id="n">Finding Anti-Hindu Websites</div>
+    ` 
+        setTimeout(() => {
+            status.innerText = "⚡ Attacking"
+            ip = localStorage.getItem('ip')
+            attack(ip)
+        }, 1000) 
+    
+}   
+function attack(link) { 
+    
+    document.getElementById("st").style.borderLeft = '2px solid green'
+    // const link = document.getElementById("url").value;
+    n = document.querySelector('.sss')
+    
     const status = document.getElementById("status")
     const sent = document.getElementById("sent")
     const attacks = document.getElementById("attacks")
     const time = document.getElementById("time")
     const sites = document.getElementById("sites")
-    c = Math.floor(Math.random())
-    if (c == 0) {
-        host = "https://"
-        localStorage.setItem('host', host)
-        c = Math.floor(Math.random())
-        if (c == 1) {
-            ext = ".in"
-            localStorage.setItem('ext', ext)
-        }
-        else if (c == 0) {
-            ext = ".com"
-            localStorage.setItem('ext', ext)
-        }
-    }
-    else if(c==1) {
-        host = "www."
-        localStorage.setItem('host', host)
-        c = Math.floor(Math.random())
-        if (c == 1) {
-            ext = ".in"
-            localStorage.setItem('ext', ext)
-        }
-        else if (c == 0) {
-            ext = ".com"
-            localStorage.setItem('ext', ext)
-        }
-    }
-    
+    // c = Math.floor(Math.random())
+    // if (c == 0) {
+    //     host = "https://"
+    //     localStorage.setItem('host', host)
+    //     c = Math.floor(Math.random())
+    //     if (c == 1) {
+    //         ext = ".in"
+    //         localStorage.setItem('ext', ext)
+    //     }
+    //     else  {
+    //         ext = ".com"
+    //         localStorage.setItem('ext', ext)
+    //     }
+    // }
+    // else if(c==1) {
+    //     host = "www."
+    //     localStorage.setItem('host', host)
+    //     c = Math.floor(Math.random())
+    //     if (c == 1) {
+    //         ext = ".in"
+    //         localStorage.setItem('ext', ext)
+    //     }
+    //     else  {
+    //         ext = ".com"
+    //         localStorage.setItem('ext', ext)
+    //     }
+    // }      
     sent.innerHTML = link;
-    for(let i = 1; i<=6000;i++) {
-        setTimeout(() => {
-            time.innerHTML = i.toFixed(2)+"s"
-            
-        }, i*1000);
+    n.innerHTML = `
+    <div class="n" id="n">Attack Started on: ${link}</div>
+    `
+    for(let i = 1; i<=5000;i++) {
+        for(let t = 1; t<=5;t++) {
+            setTimeout(() => {
+                time.innerHTML = t.toFixed(2)+"s"
+            }, t*1000);
+        }   
         setTimeout(() => {
             attacks.innerHTML = i
-            if (i == 5999) {
-                addx()
+            if (i == 5000) {  
+                
+                addx() 
                 document.getElementById("st").style.borderLeft = '2px solid red'
                 status.innerText = "🔴 OFF"
-                sl = `<div class='site'>${localStorage.getItem("host")}${link}${localStorage.getItem("ext")}</div>`
+                sl = `<div class='site' onclick='rrr("${link}")'>
+                https://${link}.host
+                </div>
+                ` 
+                s2 = `IP: ${link} | Attacks: ${Math.floor(Math.random() * 10000 - 1000)+1000}`
                 localStorage.setItem('sites', localStorage.getItem('sites')+sl);
+                localStorage.setItem('oi', localStorage.getItem('oi')+s2+"\n");
                 time.innerHTML = '0.00s'
-                link.innerHTML = '........'
+                link.innerHTML = '........' 
                 attack.innerHTML = '0'
+                
                 loadhistory();
-                window.location.reload()
-            }
-        }, i*2);
+                auto()
+            }  
+              
+        }, i*1.1)
+ 
         
-    }
+        
+         
+    }  
+   
+} 
+
+function rrr(link) {
+    console.log('dsdsadsad')
+    const msg = document.getElementById("di");
+    message = `
+    <div class='head'>
+   <h3>WEBSITE DETAILS</h3>
+   <h3 class='close' onclick="cslose()">❌</h3>
+    </div>
+    <div class='text'>
+    <table>
+                    <thead>
+                        <tr>
+                        <td>Name</td>
+                       <td>Value</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Host IP</td>
+                            <td align="center">${link}</td>
+                        </tr>
+                        <tr>
+                            <td>Status</td>
+                            <td align="center">🔴 Offline</td>
+                        </tr>
+                        <tr>
+                            <td>Data</td>
+                            <td  align="center">🗑 Deleted</td>
+                        </tr>
+                    </tbody>
+                </table>
+    </div>
+    `
+    msg.innerHTML = message;
+    document.querySelector("body").style.opacity = 0.2
+    msg.showModal()
+}
+function cslose() {
+    document.querySelector("body").style.opacity = 1
+    document.getElementById('di').close()
 }
 function loadhistory() {
     const count = localStorage.getItem('sc')
-    if (count == null || count == undefined ) {
+    if (!count) {
         localStorage.setItem('sc',0)       
-    }                
+        document.querySelector('.sc').innerHTML = `HISTORY (${count})`
+        loadhistory()
+        
+    }
+    else if(count.includes(null) == true){
+        localStorage.setItem('sc',0)       
+        document.querySelector('.sc').innerHTML = `HISTORY (${count})`
+        loadhistory()
+
+    }
     document.querySelector('.sc').innerHTML = `HISTORY (${count})`
     const sites = document.getElementById("sites")
-    sites.innerHTML = localStorage.getItem("sites").replace(null,'');
+    const iii = localStorage.getItem('sites');
+     if(!iii) {
+        localStorage.setItem('sites', Date().replace('GMT+0530 (India Standard Time)','<br>'))
+       sites.innerHTML = iii
+
+       loadhistory()
+    } 
+    else if(iii.includes(null) == true){    
+        localStorage.setItem('sites', Date().replace('GMT+0530 (India Standard Time)','<br>')) 
+       sites.innerHTML = iii
+       loadhistory()
+
+    }
+    else if (1==1) {
+        refresh()
 }
-document.addEventListener('DOMContentLoaded', loadhistory)
+    sites.innerHTML = iii
+    
+}
+function refresh() {
+    const count = localStorage.getItem('sc')
+    if (!count) {
+        localStorage.setItem('sc',0)       
+        document.querySelector('.sc').innerHTML = `HISTORY (${count})`
+        loadhistory()
+        
+    }
+    else if(count.includes(null) == true){
+        localStorage.setItem('sc',0)       
+        document.querySelector('.sc').innerHTML = `HISTORY (${count})`
+        loadhistory()
+
+    }
+    document.querySelector('.sc').innerHTML = `HISTORY (${count})`
+    const sites = document.getElementById("sites")
+    const iii = localStorage.getItem('sites');
+     if(!iii) {
+        localStorage.setItem('sites', Date().replace('GMT+0530 (India Standard Time)','<br><br><br>'))
+       sites.innerHTML = iii
+
+       refresh()
+    } 
+    else if(iii.includes(null) == true){    
+        localStorage.setItem('sites', Date().replace('GMT+0530 (India Standard Time)','<br><br><br>')) 
+       sites.innerHTML = iii
+       refresh()
+
+    }
+    sites.innerHTML = iii
+    
+}
+
 function addx() {
     count = Number(localStorage.getItem('sc'));
-    localStorage.setItem("sc", Number(count)+Number(2))
+    localStorage.setItem("sc", Number(count)+Number(1))
 }
+function auto() {
+   const fd =  Math.floor(Math.random() * (300 - 49) + 49); 
+   const sd =  Math.floor(Math.random() * (10-0) + 0); 
+   const gd =  Math.floor(Math.random() * (10 - 1) + 1); 
+   const port =  Math.floor(Math.random() * (10000 - 1000) + 1000); 
+   const ip = `${fd}.${sd}.${gd}:${port}`
+   localStorage.setItem('ip', ip)
+   start()
+}
+function cancel() {
+    localStorage.removeItem('ip')
+    n = document.querySelector('.sss')
+    n.innerHTML = `
+    <div class="n" id="n">Stopping Attack</div>
+    `
+        window.location.reload()
+    
+     
+}
+function scr() {
+    var height = document.body.scrollHeight;
+    window.scroll(0, height);
+    document.querySelector('.bt').innerText = '⬆'
+    document.querySelector('.bt').onclick = topsx
+}
+function topsx() {
+    window.scroll(0, screenTop);   
+    document.querySelector('.bt').innerText = '⬇'
+    document.querySelector('.bt').onclick = scr
+} 
+document.addEventListener('DOMContentLoaded', loadhistory)
---
+++ attack.js
@@ -0,0 +1,56 @@
+{"user":true,"userId":"45854904407","authenticated":true,"oneTapPrompt":true,"status":"ok"}
+
+
+
+
+
+
+
+HTTP/2 200 OK
+Content-Type: application/json; charset=utf-8
+Ig-Set-Password-Encryption-Web-Key-Id: 105
+Ig-Set-Password-Encryption-Web-Pub-Key: cca9aaf1836fad67a435fcd732691c92cfe0015be4e0f5eab68a93bdc3c1e077
+Ig-Set-Password-Encryption-Web-Key-Version: 10
+X-Robots-Tag: noindex
+Date: Mon, 22 Jan 2024 16:07:40 GMT
+Vary: Accept-Language, Cookie
+Content-Language: en
+Access-Control-Allow-Origin: https://www.instagram.com
+Access-Control-Allow-Credentials: true
+Strict-Transport-Security: max-age=31536000
+Cache-Control: private, no-cache, no-store, must-revalidate
+Pragma: no-cache
+Expires: Sat, 01 Jan 2000 00:00:00 GMT
+X-Frame-Options: SAMEORIGIN
+Content-Security-Policy: report-uri https://www.instagram.com/security/csp_report/; default-src 'self' https://www.instagram.com; img-src data: blob: https://*.fbcdn.net https://*.instagram.com https://*.cdninstagram.com https://*.facebook.com https://*.fbsbx.com https://*.threads.net https://*.giphy.com; font-src data: https://*.fbcdn.net https://*.instagram.com https://*.cdninstagram.com; media-src 'self' blob: https://www.instagram.com https://*.cdninstagram.com https://*.fbcdn.net; manifest-src 'self' https://www.instagram.com; script-src 'self' https://instagram.com https://www.instagram.com https://*.www.instagram.com https://*.cdninstagram.com wss://www.instagram.com https://*.facebook.com https://*.fbcdn.net https://*.facebook.net 'unsafe-inline' 'unsafe-eval' blob:; style-src 'self' https://*.www.instagram.com https://www.instagram.com 'unsafe-inline'; connect-src 'self' https://instagram.com https://www.instagram.com https://*.www.instagram.com https://graph.instagram.com https://*.graph.instagram.com https://i.instagram.com/graphql_www https://graphql.instagram.com https://*.cdninstagram.com https://api.instagram.com https://i.instagram.com https://*.i.instagram.com https://*.od.instagram.com https://i.threads.net https://*.od.threads.net wss://www.instagram.com wss://edge-chat.instagram.com https://*.facebook.com https://*.fbcdn.net https://*.facebook.net chrome-extension://boadgeojelhgndaghljhdicfkmllpafd blob:; worker-src 'self' blob: https://www.instagram.com; frame-src 'self' https://instagram.com https://www.instagram.com https://*.instagram.com https://staticxx.facebook.com https://www.facebook.com https://web.facebook.com https://connect.facebook.net https://m.facebook.com https://*.fbsbx.com; object-src 'none'; upgrade-insecure-requests
+Cross-Origin-Embedder-Policy-Report-Only: require-corp;report-to="coep"
+Report-To: {"group": "coep", "max_age": 86400, "endpoints": [{"url": "/security/coep_report/"}]},{"group": "coop", "max_age": 86400, "endpoints": [{"url": "/security/coop_report/"}]}
+Cross-Origin-Resource-Policy: cross-origin
+Cross-Origin-Opener-Policy: same-origin-allow-popups;report-to="coop"
+X-Content-Type-Options: nosniff
+X-Xss-Protection: 0
+X-Ig-Push-State: c2
+X-Ig-Cache-Control: cache
+X-Aed: 139
+X-Ig-Set-Www-Claim: hmac.AR3lofHUmeQFywS2_7tQdE_g09FRlCtoTGXl46j2qGXP_csv
+Access-Control-Expose-Headers: X-IG-Set-WWW-Claim
+X-Ig-Request-Elapsed-Time-Ms: 1853
+X-Ig-Peak-Time: 1
+Set-Cookie: csrftoken=Vd2r9xbRnAeYTLuxdA91EuSkEqR5iyEa; Domain=.instagram.com; expires=Mon, 20-Jan-2025 16:07:40 GMT; Max-Age=31449600; Path=/; Secure
+Set-Cookie: rur="CCO\05445854904407\0541737475660:01f7a5a746b62e7e0b184eb43a6bc355a549dcdf9c5cc6f6a078ceae881814600aae744d"; Domain=.instagram.com; HttpOnly; Path=/; SameSite=Lax; Secure
+Set-Cookie: ds_user_id=45854904407; Domain=.instagram.com; expires=Sun, 21-Apr-2024 16:07:40 GMT; Max-Age=7776000; Path=/; Secure
+Set-Cookie: th_eu_pref=""; Domain=instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=.instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=i.instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=.i.instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=www.instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=.www.instagram.com; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; Domain=.threads.net; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: th_eu_pref=""; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
+Set-Cookie: sessionid=45854904407%3ASeduDwGFxGnuiA%3A24%3AAYeHTBCaUIvMVG1BozZdqZYDqBLSz_bSPKZ13lt5uA; Domain=.instagram.com; expires=Tue, 21-Jan-2025 16:07:40 GMT; HttpOnly; Max-Age=31536000; Path=/; Secure
+Content-Length: 91
+X-Fb-Load: 399
+X-Ig-Origin-Region: cco
+Alt-Svc: h3=":443"; ma=86400
+
+{"user":true,"userId":"45854904407","authenticated":true,"oneTapPrompt":true,"status":"ok"}
